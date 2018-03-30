64a0d5dd0628a90173c5f1f38d95826d27b2eb15
sudarshanpanke <sudarshanpanke@gmail.com>
2018-03-30 15:47:25 +0530

    Run and tested Practice.cpp file it is working correctly

--- Microsoft/Practice.cpp
+++ Microsoft/Practice.cpp
@@ -2,29 +2,29 @@
 using namespace std;
 int getSingle(int arr[], int n)
 {
-     
-     int num = 0;
-     for(int j = 0; j<32; j++)
-     {
-	int bit = 0;
+
+	int num = 0;
 	int fact = 1;
-	for(int i = 0; i<n; i++)
+	for (int j = 0; j<32; j++)
 	{
-	   if(arr[i]&fact == fact)
-	     curr_bit++;
+		int curr_bit = 0;
+		for (int i = 0; i<n; i++)
+		{
+			if ((arr[i] & fact) == fact)
+				curr_bit++;
+		}
+		if (curr_bit % 3 == 1)
+		{
+			num = num | fact;
+		}
+		fact <<= 1;
 	}
-	if(curr_bit%3 == 1)
-	{
-	   num = num | fact;
-	}
-	fact <<= 1;
-     }
-     return num;
- }
+	return num;
+}
 int main()
 {
-   int arr[] = {3,2,3,3,1,1,1};
-   int n = sizeof(arr)/sizeof(arr[0]);
-   printf("The element with single occurrence is %d ",getSingle(arr,n));
-   return 0;
+	int arr[] = { 3,2,3,3,1,1,1,2,2,4,4,4,100 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	printf("The element with single occurrence is %d ", getSingle(arr, n));
+	return 0;
 }
