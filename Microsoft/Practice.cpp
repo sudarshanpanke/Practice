05e3ebe8d5e4ae92eda2838ca2228b4c5f30421d
sudarshanpanke <sudarshanpanke@gmail.com>
2018-03-30 15:21:53 +0530

    Creating my first file using vim editor and commiting to git repo through command line

---
+++ Microsoft/Practice.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int getSingle(int arr[], int n)
+{
+     
+     int num = 0;
+     for(int j = 0; j<32; j++)
+     {
+	int bit = 0;
+	int fact = 1;
+	for(int i = 0; i<n; i++)
+	{
+	   if(arr[i]&fact == fact)
+	     curr_bit++;
+	}
+	if(curr_bit%3 == 1)
+	{
+	   num = num | fact;
+	}
+	fact <<= 1;
+     }
+     return num;
+ }
+int main()
+{
+   int arr[] = {3,2,3,3,1,1,1};
+   int n = sizeof(arr)/sizeof(arr[0]);
+   printf("The element with single occurrence is %d ",getSingle(arr,n));
+   return 0;
+}
